33a012ca4c40c2943f9e2a5739c215419e2d3e57
benjamin-elusers <benjamin.dubreuil@weizmann.ac.il>
2026-02-01 18:22:31 +0200

    update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,7 +75,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/lab-life-logisticss">
+            to="/docs/lab-life-logistics">
             📖 Start Reading
           </Link>
         </div>
