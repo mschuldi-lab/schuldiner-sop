056122964ecfe2f63b1a302548cf5fe57d54632d
benjamin-elusers <benjamin.dubreuil@weizmann.ac.il>
2026-02-04 11:03:27 +0200

    fix navbar right side

--- src/pages/index.js
+++ src/pages/index.js
@@ -70,8 +70,6 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         
-
-
         <Heading as="h1" className="hero__title">
           Standard Operating Procedures
         </Heading>
